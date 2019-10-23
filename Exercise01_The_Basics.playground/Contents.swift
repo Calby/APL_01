8d427f4ccf9f25d13556a36b975e06d4f6bbf341
James Calby <JamesCalby@gmail.com>
2019-10-23 12:01:56 -0400

    Part 1
    Added Part of Exercise

--- Exercise01_The_Basics.playground/Contents.swift
+++ Exercise01_The_Basics.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var str = "Hello, playground"
+/* Part 1: Variable Declarations  */
+
+let explicitBool: Bool = false
+var implicitArrayOfStrings = ["The", "Array", "Of", "Strings"]
+var explicitFloat: Float = 4.44444
+//Implicitly declare an EMPTY array of Ints
+var implicitEmptyArrayOfInts = [Int]()
+let explicitConstant: UInt32 = 13
+//Implicity declare an array of arrays of Doubles
+var implicitArrayOfArraysOfDoubles = [[4.44, 5.55],[6.67, 7.77]]
